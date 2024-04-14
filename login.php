0c3278b7f573d5670f8d831d438053cb128e055e
ikramakif42 <ikramakif42@gmail.com>
2024-04-14 19:39:04 +0600

    LOGIN WORKS

--- login.php
+++ login.php
@@ -1,13 +1,5 @@
 <?php
 
-function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
-
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-}
-
 session_start();
 include('dbconn.php');
 
