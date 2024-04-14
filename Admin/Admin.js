2d312fa05a9e79077cda5e173edd3ad260db86a6
ikramakif42 <ikramakif42@gmail.com>
2024-04-14 20:25:07 +0600

    Redirection and pop script

--- Admin/Admin.js
+++ Admin/Admin.js
@@ -4,15 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
         var userID = document.getElementsByName("userID")[0].value;
         var pw = document.getElementsByName("password")[0].value;
         var xhr = new XMLHttpRequest();
-        
+
         xhr.open('POST', 'login.php');
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        console.log(xhr.responseText);
         xhr.send('userID=' + encodeURIComponent(userID) + '&password=' + encodeURIComponent(pw));
         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
-                    console.log(xhr.responseText);
                     var response = JSON.parse(xhr.responseText);
                     console.log(response);
                     console.log(userID + pw);
@@ -23,8 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         alert("Error, wrong password!");
                     }
                     else {
-                        // alert("LESGOO");
-                        window.location.href = response.url;
+                        alert("LESGOO");
                     }
                 } else {
                     alert("Request failed with status: " + xhr.status);
