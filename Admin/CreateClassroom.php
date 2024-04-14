f155aa53b934f2642416b76a0c999dc91c3bab52
dhara4876 <139842149+dhara4876@users.noreply.github.com>
2024-04-14 20:18:29 +0600

    frontend fixing

--- Admin/CreateClassroom.php
+++ Admin/CreateClassroom.php
@@ -47,12 +47,16 @@
 <body>
 
 
+
+
 <nav>
     <ul>
+        <li><a href="Dashboard.php">Dashboard</a></li>
         <li><a href="AddStudent.php">Add Student</a></li>
         <li><a href="AddTeacher.php">Add Teacher</a></li>
         <li><a href="CreateClassroom.php">Create Classroom</a></li>
         <li><a href="ViewClassrooms.php">View Classrooms</a></li>
+        <li><a href="login.php">Log Out</a></li>
     </ul>
 </nav>
 
